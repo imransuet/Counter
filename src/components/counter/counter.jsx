68152ad2a22000288fe714ffc12637644397bcf4
imransuet <imransuet@gmail.com>
2020-09-18 10:41:17 +0200

    fixed central counter

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import './counter.css'
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+    this.increment = this.increment.bind(this);
+  }
+ 
 
 
+  render() {
+    return (
+      <div className="Counter">
+
+        <CounterButton by={5} incrementMethod={this.increment} />
+        <CounterButton by={10} incrementMethod={this.increment} />
+        <CounterButton by={15} incrementMethod={this.increment} />
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+  increment(by) {
+    this.setState(
+      {
+        counter: this.state.counter + by
+      }
+    );
+  }
+
+}
+
+class CounterButton extends Component {
+
+  //Deifne initial state in a constructor of the counter
+  //
   constructor() {
     super();
     this.state = {
@@ -19,8 +53,8 @@ class Counter extends Component {
     return (
 
       <div className="counter" >
-        <button onClick={this.increment}>+1</button>
-        <span className="count">{this.state.counter}</span>
+        <button onClick={this.increment}>+{this.props.by}</button>
+        
       </div>
     )
 
@@ -29,11 +63,18 @@ class Counter extends Component {
   increment() {
     this.setState(
       {
-        counter: this.state.counter + 1
+        counter: this.state.counter + this.props.by
       }
     );
+    this.props.incrementMethod(this.props.by);
   }
 }
 
+CounterButton.defaultProps = {
 
+  by: 1
+}
+CounterButton.protoTypes = {
+  by: PropTypes.number
+}
 export default Counter;
